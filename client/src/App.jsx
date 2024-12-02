11520a460d9ec4fff71a6a76210fd14ec6c6ee64
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2024-12-02 23:46:32 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -80,7 +80,7 @@ function App() {
               onClick={clearMessages}
               className="ml-4 flex items-center justify-center rounded-md bg-blue-600 hover:bg-blue-500 text-white text-sm font-medium px-4 py-2 shadow-md transition duration-300"
             >
-              Clear All Messages 🧹
+              Clear All 🧹
             </button>
           </div>
           <ul className="bg-gray-700 p-4 rounded-lg overflow-y-auto h-96 space-y-2">
